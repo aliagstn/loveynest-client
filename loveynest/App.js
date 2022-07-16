c22dc0315b80e9dbe0c3b544aa7ce2d44bb2fe58
nourmarafita <nmarafita@gmail.com>
2022-07-16 17:59:41 +0700

    adding welcome

--- loveynest/App.js
+++ loveynest/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OnBoardScreen from "./screens/OnBoardScreen";
+// import HomeScreen from "./screens/HomeScreen";
+// import DetailScreen from "./screens/DetailScreen";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="OnBoardScreen" component={OnBoardScreen} />
+        {/* <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="DetailScreen" component={DetailScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ loveynest/consts/colors.js
@@ -0,0 +1,11 @@
+const COLORS = {
+    white: "#FFF",
+    dark: "#4b5563",
+    light: "#f6f6f6",
+    grey: "#9ca3af",
+    blue: "#5F82E6",
+    red: "red",
+    transparent: "rgba(0,0,0,0)",
+}
+
+export default COLORS
